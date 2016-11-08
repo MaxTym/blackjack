55988a2559581dc1c8804bec8f9ef93417a317a5
Maksym Tymtsiv <mtymtsiv@gmail.com>
2016-11-07 21:32:06 -0500

    adjusted ace value

--- blackjack.py
+++ blackjack.py
@@ -1,4 +1,6 @@
 import random
+import os
+
 
 class Card:
 
@@ -51,10 +53,10 @@ class Deck:
 
 
 def deal():
+    account = 100
+    print("Welcome to Blackjack!!!\nYour account: {}$".format(account))
     d = Deck()
     d.shuffle()
-    #print(d[0].rank)
-    #print(d[1].rank)
     points = 0
     dealer_points = 0
     hand = []
@@ -62,35 +64,73 @@ def deal():
     current_card = d[0]
     hand.append(current_card)
     d = (d[1:])
-    points += count_points(current_card)
     current_card = d[0]
     dealer_hand.append(current_card)
-    print("Dealer's hand: {}#".format(current_card))
+    print("Dealer's hand: {} #".format(current_card))
     d = (d[1:])
-    dealer_points += count_points(current_card)
     current_card = d[0]
     hand.append(current_card)
+    get_points(hand)
     d = (d[1:])
-    points += count_points(current_card)
-    print(hand)
-    print(points)
+    print("Your hand: {}".format(hand))
+    points = get_points(hand)
+    print("Your points: {}".format(points))
     current_card = d[0]
     dealer_hand.append(current_card)
     d = (d[1:])
-    dealer_points += count_points(current_card)
+    dealer_points = get_points(dealer_hand)
     if points < 21:
-        while another_card():
-            hand.append(d[0])
+        while points < 22:
+            hit = input("One more card? 'Y'/'n'\n").lower()
+            if hit == 'y':
+                current_card = d[0]
+                hand.append(current_card)
+                d = (d[1:])
+                if check_for_ace(hand):
+                    if adjust_ace_value(hand):
+                        points = get_points(hand)
+                    else:
+                        points = get_points_a1(hand)
+                else:
+                    points = get_points(hand)
+                print(current_card)
+                print("Your hand: {}".format(hand))
+                print("Your points: {}".format(points))
+            elif hit == 'n':
+                break
+            else:
+                continue
+        else:
+            print("You lose!\n You have {} points".format(points))
+            account -= 10
+            print(hand)
+            if play_again():
+                deal()
+
+        while dealer_points < 17:
             current_card = d[0]
-            d = (d[1:])
-            points += count_points(current_card)
-            print(points)
-        while dealer_points <17:
             dealer_hand.append(current_card)
             d = (d[1:])
-            dealer_points += count_points(current_card)
+            if check_for_ace(dealer_hand):
+                if adjust_ace_value(dealer_hand):
+                    points = get_points(dealer_hand)
+                else:
+                    dealer_points = get_points_a1(dealer_hand)
+            else:
+                dealer_points = get_points(dealer_hand)
+        if dealer_points > 21:
+            print("Congratulations!!! You won")
+            print('Dealer points: {}'.format(dealer_points))
+            print('Dealer cards: {}'.format(dealer_hand))
+            print('Your points: {}'.format(points))
+            print('Your cards: {}'.format(hand))
+            account += 10
+            if play_again():
+                deal()
+
         if dealer_points > points:
             print("You lose")
+            account -= 10
         elif dealer_points == points:
             print("Its a draw")
         else:
@@ -99,12 +139,17 @@ def deal():
         print('Dealer cards: {}'.format(dealer_hand))
         print('Your points: {}'.format(points))
         print('Your cards: {}'.format(hand))
+        account += 10
+        if play_again():
+            deal()
     elif points > 21:
-
         print("You lose")
+        if play_again():
+            deal()
     else:
         print("You won!!!")
-
+        if play_again():
+            deal()
 
 
 def another_card():
@@ -119,9 +164,6 @@ def another_card():
         else:
             continue
 
-def dealer_turn():
-    dealer_points = 0
-
 
 def count_points(current_card):
     if current_card.rank == 'J' or current_card.rank == 'Q' or current_card.rank == 'K':
@@ -132,5 +174,74 @@ def count_points(current_card):
         points = int(current_card.rank)
     return points
 
-deal()
+
+def get_points(hand):
+    total = 0
+    for i in hand:
+        if i.rank == 'J' or i.rank == 'Q' or i.rank == 'K':
+            points = 10
+        elif i.rank == 'A':
+            points = 11
+        else:
+            points = int(i.rank)
+        total += points
+    return total
+
+
+def get_points_a1(hand):
+    total = 0
+    for i in hand:
+        if i.rank == 'J' or i.rank == 'Q' or i.rank == 'K':
+            points = 10
+        elif i.rank == 'A':
+            points = 1
+        else:
+            points = int(i.rank)
+        total += points
+    return total
+
+
+def adjust_ace_value(hand):
+    if get_total_of_non_ace_cards(hand) <= 10:
+        return True
+
+
+def get_total_of_non_ace_cards(hand):
+    total = 0
+    for i in hand:
+        if i.rank != 'A':
+            if i.rank == 'J' or i.rank == 'Q' or i.rank == 'K':
+                points = 10
+            else:
+                points = int(i.rank)
+            total += points
+    print(total)
+    return total
+
+
+def play_again():
+    while True:
+        play_again = input("Anoher round? 'Y'/'n'").lower()
+        if play_again == 'y':
+            return True
+            break
+        elif play_again == 'n':
+            exit()
+
+
+def check_for_ace(hand):
+    for i in hand:
+        if i.rank == 'A':
+            return True
+
+
+
+def main():
+    os.system('clear')
+    deal()
+
+
+#deal()
 #count_points()
+
+main()
